5c05de2b435f7e60deb51d195363d4181bf33a13
KLB <k.besser@tu-bs.de>
2022-08-02 16:06:25 +0200

    Add additional tests for the solution algorithms
    Improve the unit testing of the implemented solution algorithms. This
    includes a exception test

---
+++ tests/test_algorithm_fcs.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pytest
+
+from qmkpy import total_profit_qmkp
+from qmkpy.algorithms import fcs_procedure, constructive_procedure
+
+@pytest.mark.parametrize("alpha", (None, .5, .1, .999, .2))
+def test_fcs_with_alpha(alpha):
+    num_elements = 8
+    num_knapsacks = 3
+    profits = np.random.randint(0, 8, size=(num_elements, num_elements))
+    profits = profits @ profits.T
+    weights = np.random.randint(1, 5, size=(num_elements,))
+    capacities = np.random.randint(3, 12, size=(num_knapsacks,))
+    solution = fcs_procedure(profits, weights, capacities,
+                             alpha=alpha)
+    total_profit = total_profit_qmkp(profits, solution)
+    assert np.all(np.shape(solution) == (num_elements, num_knapsacks)) and total_profit >= 0
+
+@pytest.mark.parametrize("len_history", (1, 10, 15, 20, 50))
+def test_fcs_with_history(len_history):
+    num_elements = 8
+    num_knapsacks = 3
+    profits = np.random.randint(0, 8, size=(num_elements, num_elements))
+    profits = profits @ profits.T
+    weights = np.random.randint(1, 5, size=(num_elements,))
+    capacities = np.random.randint(3, 12, size=(num_knapsacks,))
+    solution = fcs_procedure(profits, weights, capacities,
+                             len_history=len_history)
+    total_profit = total_profit_qmkp(profits, solution)
+    assert np.all(np.shape(solution) == (num_elements, num_knapsacks)) and total_profit >= 0
+
+@pytest.mark.parametrize("alpha", (0, 1, -0.2, 1.2))
+def test_fcs_alpha_feasible(alpha):
+    num_elements = 8
+    num_knapsacks = 3
+    profits = np.random.randint(0, 8, size=(num_elements, num_elements))
+    profits = profits @ profits.T
+    weights = np.random.randint(1, 5, size=(num_elements,))
+    capacities = np.random.randint(3, 12, size=(num_knapsacks,))
+    with pytest.raises(ValueError):
+        solution = fcs_procedure(profits, weights, capacities,
+                                 alpha=alpha)
+
+@pytest.mark.parametrize("len_history", (0, .1, -20))
+def test_fcs_history_feasible(len_history):
+    num_elements = 8
+    num_knapsacks = 3
+    profits = np.random.randint(0, 8, size=(num_elements, num_elements))
+    profits = profits @ profits.T
+    weights = np.random.randint(1, 5, size=(num_elements,))
+    capacities = np.random.randint(3, 12, size=(num_knapsacks,))
+    with pytest.raises(ValueError):
+        solution = fcs_procedure(profits, weights, capacities,
+                                 len_history=len_history)
+
+@pytest.mark.parametrize("alpha", (None, .5, .1, .999, .2))
+def test_fcs_compare_cp(alpha):
+    num_elements = 8
+    num_knapsacks = 3
+    profits = np.random.randint(0, 8, size=(num_elements, num_elements))
+    profits = profits @ profits.T
+    weights = np.random.randint(1, 5, size=(num_elements,))
+    capacities = np.random.randint(3, 12, size=(num_knapsacks,))
+    sol_fcs = fcs_procedure(profits, weights, capacities, alpha=alpha)
+    profit_fcs = total_profit_qmkp(profits, sol_fcs)
+    sol_cp = constructive_procedure(profits, weights, capacities)
+    profit_cp = total_profit_qmkp(profits, sol_cp)
+    assert profit_fcs >= profit_cp
--- tests/test_algorithm_robin.py
+++ tests/test_algorithm_robin.py
@@ -2,10 +2,10 @@ import numpy as np
 import pytest
 
 from qmkpy import total_profit_qmkp
-from qmkpy.algorithms import constructive_procedure
+from qmkpy.algorithms import round_robin
 
 
-def test_cp_with_starting():
+def test_rr_with_starting():
     profits = np.array([[1, 1, 2, 3],
                         [1, 1, 4, 5],
                         [2, 4, 2, 6],
@@ -16,14 +16,12 @@ def test_cp_with_starting():
                                     [0, 1, 0],
                                     [0, 0, 0],
                                     [1, 0, 0]])
-    solution = constructive_procedure(profits, weights, capacities,
-                                      starting_assignment=starting_assignment)
-    print(solution)
+    solution = round_robin(profits, weights, capacities,
+                           starting_assignment=starting_assignment)
     total_profit = total_profit_qmkp(profits, solution)
-    print(total_profit)
     assert np.all(np.shape(solution) == (len(weights), len(capacities))) and total_profit > 0
 
-def test_cp_change_with_starting():
+def test_rr_change_with_starting():
     profits = np.array([[1, 1, 2, 3],
                         [1, 1, 4, 5],
                         [2, 4, 2, 6],
@@ -34,8 +32,8 @@ def test_cp_change_with_starting():
                                     [0, 1, 0],
                                     [0, 0, 0],
                                     [1, 0, 0]])
-    solution = constructive_procedure(profits, weights, capacities,
-                                      starting_assignment=starting_assignment)
+    solution = round_robin(profits, weights, capacities,
+                           starting_assignment=starting_assignment)
     _new_assignments = solution - starting_assignment
     assert np.all(_new_assignments >= 0)
 
@@ -46,7 +44,7 @@ def test_cp_change_with_starting():
                          [[1, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0]],
                          [[0, 0, 1], [0, 0, 1], [1, 0, 0], [0, 0, 1]],
                          ))
-def test_cp_feasibility_starting_assignment(starting_assignment):
+def test_rr_feasibility_starting_assignment(starting_assignment):
     profits = np.array([[1, 1, 2, 3],
                         [1, 1, 4, 5],
                         [2, 4, 2, 6],
@@ -55,5 +53,5 @@ def test_cp_feasibility_starting_assignment(starting_assignment):
     capacities = [5, 5, 3]
     starting_assignment = np.array(starting_assignment)
     with pytest.raises(ValueError):
-        solution = constructive_procedure(profits, weights, capacities,
-                                          starting_assignment=starting_assignment)
+        solution = round_robin(profits, weights, capacities,
+                               starting_assignment=starting_assignment)
